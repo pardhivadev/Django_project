eae21dc581c44e45daef94b46ba9d5c0bee3bdd5
pardhivas <pardhivas@gmail.com>
2021-07-26 23:58:09 +0530

    Creation of templates and passing data to them

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 17, 2018'
+    },
+    {
+        'author': 'Pardhiva',
+        'title': 'Blog post 3',
+        'content': 'Third post content',
+        'date_posted': 'April 21, 2019'
+    }
+
+]
 
 
 def home(request):
-  return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'myPosts': posts
+    }
+    return render(request, 'blog/home.html', context)
+    # return HttpResponse('<h1>Blog Home</h1>')
 
 
 def about(request):
-  return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
+    # return HttpResponse('<h1>Blog About</h1>')
